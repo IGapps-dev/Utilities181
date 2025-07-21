cb1bc792a5b985c70de1ef106672eb5609e755d8
nzhukov <nzhukov.rnd@gmail.com>
2025-07-21 13:00:03 +0400

    Initial Commit

---
+++ EmojiMaze/EmojiMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiMazeApp.swift
+//  EmojiMaze
+//
+//  Created by Nikolay Zhukov on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct EmojiMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
